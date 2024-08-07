cb0eafd0e7c9cfa0546ff893d98c1a81a54d5b58
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-07 17:49:46 +0530

    added framer motion

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,6 +1,9 @@
+import { useRef } from "react";
 import Card from "./Card";
 
 function Foreground() {
+  const ref = useRef(null);
+
   const data = [
     {
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quia.",
@@ -46,9 +49,12 @@ function Foreground() {
 
   return (
     <>
-      <div className="fixed z-[3] left-0 top-0 w-full h-full flex gap-10 flex-wrap p-5">
+      <div
+        ref={ref}
+        className="fixed z-[3] left-0 top-0 w-full h-full flex gap-10 flex-wrap p-5"
+      >
         {data.map((item, index) => (
-          <Card key={index} data={item} />
+          <Card key={index} data={item} reference={ref} />
         ))}
       </div>
     </>
