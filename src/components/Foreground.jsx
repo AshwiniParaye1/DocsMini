afb3b54612446faaf273a8a98304fa84912d176b
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-07 17:38:27 +0530

    cards created

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,55 @@
 import Card from "./Card";
 
 function Foreground() {
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quia.",
+      filesize: ".7mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quia.",
+      filesize: ".7mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "blue",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quia.",
+      filesize: ".7mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quia.",
+      filesize: ".7mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "green",
+      },
+    },
+  ];
+
   return (
     <>
-      <div className="fixed z-[3] left-0 top-0 w-full h-full">
-        <Card />
+      <div className="fixed z-[3] left-0 top-0 w-full h-full flex gap-10 flex-wrap p-5">
+        {data.map((item, index) => (
+          <Card key={index} data={item} />
+        ))}
       </div>
     </>
   );
